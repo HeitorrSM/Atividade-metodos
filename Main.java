63827309866842b897392c60bb4cc8f067ed7440
HeitorrSM <heitorsm477@gmail.com>
2026-08-16 22:44:36 +0000

    Calculadora Pronta

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculadora.soma(10, 12);
+        Calculadora.subtracao(100, 120);
+        Calculadora.multiplicacao(3, 5);
+        Calculadora.divisao(36, 6);
+    }
+}
